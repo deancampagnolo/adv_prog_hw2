51155051a4854d60e146c0c23afb2846369922a3
Dean Campagnolo <deancampagnolo@gmail.com>
2020-02-02 14:54:04 -0800

    Auto Sender

--- asg2-shell-fnptrs-oop/code/file_sys.cpp
+++ asg2-shell-fnptrs-oop/code/file_sys.cpp
@@ -137,10 +137,11 @@ inode_ptr directory::mkdir (const string& dirname) {
       make_shared<inode>(file_type::DIRECTORY_TYPE);
    new_inode_ptr->set_parent(current_inode);
    cout<<"new_inode_ptr"<<new_inode_ptr<<endl;
-   dirents.insert({dirname,new_inode_ptr});
+   weak_ptr<inode> weak_inode_ptr= new_inode_ptr;
+   dirents.insert({dirname,weak_inode_ptr});
    // TODO(me) need to sort dirents
    DEBUGF ('i', dirname);
-   return nullptr;
+   return new_inode_ptr;
 }
 
 inode_ptr directory::mkfile (const string& filename) {
