840b11d2eb2435fe1cde0764150b504ba30bff81
Dean Campagnolo <deancampagnolo@gmail.com>
2020-02-02 01:37:20 -0800

    Auto Sender

--- asg2-shell-fnptrs-oop/code/main.cpp
+++ asg2-shell-fnptrs-oop/code/main.cpp
@@ -54,8 +54,9 @@ int main (int argc, char** argv) {
       make_shared<inode>(file_type::DIRECTORY_TYPE);
    cout<<root<<endl;
    
-   /*
+   
    root->set_parent(root);
+   /*
    root->set_base_file_inode(root);
 
    state.set_root(root);
