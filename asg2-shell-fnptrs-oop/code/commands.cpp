531c9526d7ac1be13d2054b29839acad6e8b3aca
Dean Campagnolo <deancampagnolo@gmail.com>
2020-02-04 01:03:14 -0800

    Auto Sender

--- asg2-shell-fnptrs-oop/code/commands.cpp
+++ asg2-shell-fnptrs-oop/code/commands.cpp
@@ -285,9 +285,10 @@ void fn_make (inode_state& state, const wordvec& words){
 }
 
 void fn_mkdir (inode_state& state, const wordvec& words){
+   
+   string s_target = clean_cd_to_command(state, words, false);
    auto dirents = state.get_cwd_ptr()->get_base_file_ptr()
       ->get_dirents();
-   string s_target = clean_cd_to_command(state, words, false);
    if (words.size() > 1 && dirents.find(s_target) == dirents.end()) {
       wordvec origword = words;
       
