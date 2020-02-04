1761e427e0ffd874906e2ee809e6934f51ef4c4a
Dean Campagnolo <deancampagnolo@gmail.com>
2020-02-04 00:35:47 -0800

    Auto Sender

--- asg2-shell-fnptrs-oop/code/commands.cpp
+++ asg2-shell-fnptrs-oop/code/commands.cpp
@@ -274,10 +274,17 @@ void fn_make (inode_state& state, const wordvec& words){
 }
 
 void fn_mkdir (inode_state& state, const wordvec& words){
-   wordvec origword = words;
-   string s_target = clean_cd_to_command(state, words, false);
-   state.get_cwd_ptr()->get_base_file_ptr()->mkdir(s_target);
-   cd_back_command(state, origword, false);
+   auto dirents = state.get_cwd_ptr()->get_base_file_ptr()
+      ->get_dirents();
+
+   if (words.size() > 1 && dirents.find(words.at(1)) == dirents.end()) {
+      wordvec origword = words;
+      string s_target = clean_cd_to_command(state, words, false);
+      state.get_cwd_ptr()->get_base_file_ptr()->mkdir(s_target);
+      cd_back_command(state, origword, false);
+   } else {
+      complain()<<"cannot make this"<<endl;
+   }
 }
 
 void fn_prompt (inode_state& state, const wordvec& words){
@@ -331,7 +338,7 @@ void fn_rm (inode_state& state, const wordvec& words){
          cout<< "you cant do that!"<< endl;   
       }
    } else {
-      cout<<"rm file doesn't exist"<<endl;
+      complain()<<"rm file doesn't exist"<<endl;
    }
    cd_back_command(state, origword, false);
 }
