d5211f4e3d8cbb83df5c8d1866047b4224b5f7b7
Dean Campagnolo <deancampagnolo@gmail.com>
2020-02-03 16:07:46 -0800

    Auto Sender

--- asg2-shell-fnptrs-oop/code/commands.cpp
+++ asg2-shell-fnptrs-oop/code/commands.cpp
@@ -139,6 +139,7 @@ void fn_pwd (inode_state& state, const wordvec& words){
       ->get_inode_nr()){
       the_pwd.push(the_inode->get_base_file_ptr()->get_name());
       the_inode = the_inode->get_parent().lock();
+      cout<<"added one"<<endl;
    }
    string final_pwd = "/";
    int stack_size = the_pwd.size();
