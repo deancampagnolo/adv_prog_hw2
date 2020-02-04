9b476084241ba0462462d9077bb08c4fef858804
Dean Campagnolo <deancampagnolo@gmail.com>
2020-02-03 22:01:36 -0800

    Auto Sender

--- asg2-shell-fnptrs-oop/code/commands.cpp
+++ asg2-shell-fnptrs-oop/code/commands.cpp
@@ -170,27 +170,12 @@ void fn_ls (inode_state& state, const wordvec& words){
 
 void fn_lsr (inode_state& state, const wordvec& words){
    wordvec origword = words;
-   bool isroot = false;
-   if (words.size()>1 && words.at(1) == "/") {
-      isroot = true;
-   }
+   clean_cd_to_command(state, words, true);
+   map<string,inode_ptr> the_dirents = state.get_cwd_ptr()->
+      get_base_file_ptr()->get_dirents();
+   string ls_pwd = get_pwd(state,words).append(":");
+   cout<<ls_pwd<<endl;
 
-   map<string,inode_ptr> the_dirents;
-   if (!isroot) {
-      clean_cd_to_command(state, words, true);
-      the_dirents = state.get_cwd_ptr()->
-         get_base_file_ptr()->get_dirents();
-   } else {
-      the_dirents = state.get_root_ptr()->
-         get_base_file_ptr()->get_dirents();
-   }
-
-   if (!isroot) {
-      string ls_pwd = get_pwd(state,words).append(":");
-      cout<<ls_pwd<<endl;
-   } else { cout<<"/:"<<endl;}
-
-   cout<<"aych"<<endl;
    for (auto pair : the_dirents) {
       string name = pair.first;
       if (pair.second->get_base_file_ptr()->get_identity() ==
@@ -216,9 +201,7 @@ void fn_lsr (inode_state& state, const wordvec& words){
       }
 
    }
-   if (!isroot) {
-      cd_back_command(state, origword, true);
-   }
+   cd_back_command(state, origword, true);
 }
 
 void fn_make (inode_state& state, const wordvec& words){
